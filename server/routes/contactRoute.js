23947c029d94eeb1e42e2d91baa80b3efcbd5150
ankit980533 <77059477+ankit980533@users.noreply.github.com>
2024-02-05 11:02:57 +0530

    cors errore

--- server/routes/contactRoute.js
+++ server/routes/contactRoute.js
@@ -2,7 +2,13 @@ const express = require("express");
 const { contactUs } = require("../controllers/contactController");
 const router = express.Router();
 const protect = require("../middleWare/authMiddleware");
-
+router.get("/", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+     });
 router.post("/", protect, contactUs);
 
 module.exports = router;
