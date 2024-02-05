2e288932cecf4ada36036dcf360d47bced4e3573
ankit980533 <77059477+ankit980533@users.noreply.github.com>
2024-02-05 11:17:33 +0530

    cors errore

--- server/server.js
+++ server/server.js
@@ -17,8 +17,23 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors({
+//   origin: ["http://localhost:3000", "https://trinetra-inventory-management-software-4j77tf99n-ankit980533.vercel.app/"],
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true,
+//   optionsSuccessStatus: 204,
+// }));
+const corsOptions = {
+  origin: 'http://localhost:3000', // replace with your frontend origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
+app.use(cors(corsOptions));
+
+// Handle preflight requests
+app.options('*', cors(corsOptions));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Routes Middleware
