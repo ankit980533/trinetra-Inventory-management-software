d2fb48d602ab13ec6c155393da91b8d212003b1c
ankit980533 <77059477+ankit980533@users.noreply.github.com>
2024-04-01 18:16:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://trinetra-inventory-management-software-d6pb.vercel.app"],
+  origin: ["https://trinetra-inventory-management-software-d6pb.vercel.app"],
   credentials: true 
 };
     
