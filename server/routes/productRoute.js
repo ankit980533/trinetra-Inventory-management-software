511c4ae226ac2e0be6644c6f4a2cca64f1fe2cd2
ankit980533 <77059477+ankit980533@users.noreply.github.com>
2024-04-01 14:43:57 +0530

    msg eer

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -12,7 +12,7 @@ const { upload } = require("../utils/fileUpload");
 
 router.post("/", protect, upload.single("image"), createProduct);
 router.patch("/:id", protect, upload.single("image"), updateProduct);
-router.get("/", protect, getProducts);
+router.get("/", getProducts);
 router.get("/:id", protect, getProduct);
 router.delete("/:id", protect, deleteProduct);
 
