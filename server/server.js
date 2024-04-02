ded768e13702bf7b7389734a17ec3455f4e4bf7e
ankit980533 <77059477+ankit980533@users.noreply.github.com>
2024-04-02 11:46:48 +0530

    try one more

--- server/server.js
+++ server/server.js
@@ -17,19 +17,35 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// const corsOptions = {
+//    origin: ["http://localhost:3000", "https://trinetra-inventory-management-software-d6pb.vercel.app"],
+//   credentials: true 
+// };
+
+// app.use(cors(corsOptions));
+
+// app.options("*", cors(corsOptions));
+
+const allowedOrigins = ["https://trinetra-inventory-management-software-d6pb.vercel.app", "http://localhost:3000"];
+
+// Configure CORS options
 const corsOptions = {
-   origin: ["http://localhost:3000", "https://trinetra-inventory-management-software-d6pb.vercel.app"],
-  credentials: true 
+  origin: function (origin, callback) {
+    // Check if the origin is allowed or if it's a CORS preflight request
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true // Include credentials in CORS requests (cookies, authorization headers, etc.)
 };
 
-
-
-
+// Use CORS middleware with options
 app.use(cors(corsOptions));
 
+// Handle preflight requests for all routes
 app.options("*", cors(corsOptions));
-
-
 
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
